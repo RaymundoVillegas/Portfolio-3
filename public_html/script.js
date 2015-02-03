f434cbb4d760dc8ca84f3b637986b2337869eb8e
Raymundo Villegas <memovill@live.com>
2015-02-03 10:59:03 -0800

    final

--- public_html/script.js
+++ public_html/script.js
@@ -1,15 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+//-----------------------------------------------------------------------------
+//                            Document 
+//-----------------------------------------------------------------------------
 $("document").ready(function() {
     $(".blank").css("background-color", "white");
-    
+// The accordion is foldabe paragraph
     $('#superhuman').accordion({header: "h3"});
+    // This line of code is what is setting the width of the accordion 
     $('#superhuman').css('width', '400px');
-    
+
     $('h3').css('background', 'cyan');
 
     $('div:last p:nth-child(3)').css('background-color', 'pink');
@@ -17,54 +16,60 @@ $("document").ready(function() {
     $('p:last').css('color', 'red');
 
     $('p:gt(4)').css('border-style', 'solid');
-
+//Sets the background color to the first paragraph 
     $('p:first').css({'background-color': 'cyan', 'color': 'white'});
 
     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
 
     $('h1').bind('click', mouseClick);
-    
+
     $('#replaceWText').bind('click', replaceWText);
-    
+
     $('#randpara').bind('click', removeAPara);
-    
+
     $('#removePara').bind('click', removeAPara);
-    
+
+    $('#addPara').bind('click', addAPara);
+
+//Hides function when clicked
     $("#hide").click(function() {
         $("p").hide();
     });
-
+//Shows function when clicked
     $("#show").click(function() {
         $("p").show();
-    }); 
-    
+    });
+
 });
 
-
+//----------------------------------------------------------------------------
+//                             Functions
+//-----------------------------------------------------------------------------
 function showThePage( ){
     
-}   
+}    
 
+//removes a paragraph
 function removeAPara(){
-    $('#randPara p:last').remove();
+    $('#RandPara p:last').remove();
 }
-
+//adds a paragraph
 function addAPara (){
-    $('#randPara').append('<p>ADDED</p>');
+    $('#RandPara').append('<p>ADDED</p>');
 }
-
+//replaces text with another word
 function replaceWText(){
     $('#replaceWText').text('Replaced1');
 }
-
+//changes the selected id when mouse is over it
 function mouseOverMe(){
     $('h1').html("bye");
 }
-
+//Changes the selected id when mouse is not on it
 function mouseOutMe(){
     $('h1').html("hiii");
 }
-
+//Changes selected id when clicked
 function mouseClick(){
 
     $('p').html('clicked');
